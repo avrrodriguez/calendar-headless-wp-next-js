1ee8bced5360d4dafc20044c06d9e4802c01e353
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-06-14 17:12:07 -0400

    Adds 7 columnds and 5 rows to the calendar component with custome styling

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,3 +1,4 @@
+import { responsePathAsArray } from "graphql"
 import GetCalendarMonthDays from "helpers/GetCalendarMonthDays"
 import Header from "./Header"
 
@@ -5,15 +6,27 @@ export default function Calendar(): JSX.Element {
 
 
   return (
-    <div className="grid-container" style={{height: "100vh"}}>
-        <div className="row">
-          <h1 className="col">calendar goes here</h1>
+    <div>
+        <div className="calendar-header">
+          <h1>Calendar</h1>
         </div>
-        {[...Array(GetCalendarMonthDays("June")).keys()].map((number)=>{
-          return <div className="col" id={number.toString()} key={number+1}>
-            {number+1}
-          </div>
-        })}
+        <div className="calendar-days" style={{display: "grid", gridTemplateColumns: "repeat(7, 1fr)", columnGap: "10px"}}>
+        {
+          [...Array(7).keys()].map((col) => {
+            return(
+              <div className="col" style={{display: "grid", backgroundColor: "orange", rowGap:"10px"}} id={`col-${col}`} key={`col-${col}`}>
+                 {
+                  [...Array(5).keys()].map((row)=>{
+                    return(
+                      <div className="row" style={{backgroundColor: "green", height: "115px", width: "110px"}} id={`col-${col}-row-${row}`} key={`col-${col}-row-${row}`}>1</div>
+                    )
+                  })
+                 } 
+              </div>
+            )
+          })
+        }
+        </div>
     </div>
   )
   }
