25f1e5a9426a3cc42dbc12d9074749ace52bebac
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-06-15 21:59:40 -0400

    adds days of the week div, as well as proper spacing between calendar days, also background colors and borders

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -6,19 +6,38 @@ export default function Calendar(): JSX.Element {
 
 
   return (
-    <div>
-        <div className="calendar-header">
+    <div style={{backgroundColor: "#967bb6", height: "100vh"}}>
+        <div className="calendar-header" style={{display: "flex", justifyContent: "center"}}>
           <h1>Calendar</h1>
         </div>
-        <div className="calendar-days" style={{display: "grid", gridTemplateColumns: "repeat(7, 1fr)", columnGap: "10px"}}>
+        <div className="weekdays" style={{display:"grid", gridTemplateColumns: "repeat(7, 1fr)", justifyItems: "center"}}>
+          <div>Sunday</div>
+          <div>Monday</div>
+          <div>Tuesday</div>
+          <div>Wednesday</div>
+          <div>Thrusday</div>
+          <div>Friday</div>
+          <div>Saturday</div>
+        </div>
+        <div className="calendar-days" style={{display: "grid", gridTemplateColumns: "repeat(7, 1fr)", justifyItems: "center"}}>
         {
           [...Array(7).keys()].map((col) => {
             return(
-              <div className="col" style={{display: "grid", backgroundColor: "orange", rowGap:"10px"}} id={`col-${col}`} key={`col-${col}`}>
+              <div className="col" style={{display: "grid"}} id={`col-${col}`} key={`col-${col}`}>
                  {
                   [...Array(5).keys()].map((row)=>{
                     return(
-                      <div className="row" style={{backgroundColor: "green", height: "115px", width: "110px"}} id={`col-${col}-row-${row}`} key={`col-${col}-row-${row}`}>1</div>
+                      <div 
+                      className="row" 
+                      style={{
+                        border: "2px solid gray", 
+                        height: "115px", 
+                        width: "110px", 
+                        backgroundColor: "white"
+                      }} 
+                      id={`col-${col}-row-${row}`} 
+                      key={`col-${col}-row-${row}`}
+                      ></div>
                     )
                   })
                  } 
