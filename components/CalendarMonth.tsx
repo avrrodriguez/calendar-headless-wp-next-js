009b36762836938c06b7ca4e09215ef267762937
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-06-21 18:21:19 -0400

    puts where the days of the calendar are set, such as numbers and background color

--- components/CalendarMonth.tsx
+++ components/CalendarMonth.tsx
@@ -1,42 +1,8 @@
-import GetCalendarMonthDays from "helpers/GetCalendarMonthDays"
-import { useEffect } from "react"
 import { dateMonthYear } from "types/graphql/graphql"
-import { Event } from "types/graphql/graphql";
 import { WeekDays } from "./WeekDays";
-import { CalendarDays } from "./CalendarDays";
+import { SetCalendarDays } from "./SetCalendarDays";
 
 export default function CalendarMonth({ month, year }: dateMonthYear): JSX.Element {
-  const date: Date = new Date(`${month} 1, ${year}`)
-  const startMonthDay: Number = date.getDay()
-  var endMonthDay = GetCalendarMonthDays(`${month}`)
-  var day = 1
-
-  let anEvent: Event = {
-    eventType: "holiday",
-    eventName: "Ram",
-    eventStartDate: "05/05/2023",
-    eventEndDate: "05/05/2023",
-    eventDescription: "An even that celebrates something"
-  }
-
-  function CalendarDay(): void {
-    [...Array(6).keys()].map((row) => {
-      [...Array(7).keys()].map((col)=>{
-          if(((col >= startMonthDay && row == 0) || row > 0) && day <= endMonthDay) {
-            document.getElementById(`col-${col}-row-${row}`).innerText = day.toString()
-            day++
-          } else {
-            document.getElementById(`col-${col}-row-${row}`).style.backgroundColor = "gray"
-          }
-      })
-    })
-  }
-
-  // make a separate component calendarday in which the id of the day div (col-#-row-#) gets passed, do everything there
-
-  function addEvents(event: Event): void {}
-
-  useEffect(() => {CalendarDay()})
 
   return (
     <div>
@@ -53,14 +19,15 @@ export default function CalendarMonth({ month, year }: dateMonthYear): JSX.Eleme
                       className="row" 
                       style={{
                         border: "2px solid gray", 
-                        height: "105px", 
+                        height: "100px", 
                         width: "110px", 
                         backgroundColor: "white"
                       }} 
                       id={`col-${col}-row-${row}`} 
                       key={`col-${col}-row-${row}`}
-                      >
-                        <CalendarDays calendarDayId={`col-${col}-row-${row}`} ></CalendarDays>
+                      > 
+                      <SetCalendarDays calendarDayId={`col-${col}-row-${row}`} month={month} year={year}></SetCalendarDays>
+                      
                       </div>
                     )
                   })
---
+++ components/SetCalendarDays.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react"
+import GetCalendarMonthDays from "helpers/GetCalendarMonthDays"
+
+type typeOfCalendarDayd = {
+  calendarDayId: String
+  month: String
+  year: String
+}
+
+export function SetCalendarDays({ calendarDayId, month, year }: typeOfCalendarDayd): JSX.Element {
+  console.log(calendarDayId)
+  const date: Date = new Date(`${month} 1, ${year}`)
+  const startMonthDay: Number = date.getDay()
+  var endMonthDay = GetCalendarMonthDays(`${month}`)
+  var day = 1
+  
+  function displayDay() {
+    [...Array(6).keys()].map((row) => {
+      [...Array(7).keys()].map((col)=>{
+          if(((col >= startMonthDay && row == 0) || row > 0) && day <= endMonthDay) {
+            document.getElementById(`col-${col}-row-${row}`).innerText = day.toString()
+            day++
+          } else {
+            document.getElementById(`col-${col}-row-${row}`).style.backgroundColor = "gray"
+          }
+      })
+    })
+  }
+
+  useEffect(() => {displayDay()})
+
+  return (
+    <div></div>
+  )
+}
+
