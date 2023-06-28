ec5e2a9fc3aa10053639c36607432ce92d263147
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-06-28 16:29:07 -0400

    Adds clicks to past month and future month on click of respective buttons

--- components/CalendarAllMonths.tsx
+++ components/CalendarAllMonths.tsx
@@ -8,22 +8,43 @@ export default function CalendarAllMonths(): JSX.Element {
     year: "2023"
   }
 
-  function ChangeMonth(){}
+  function ChangeToPastMonth(){
+    console.log("past month")
+  }
+
+  function ChangeToFutureMonth(){
+    console.log("future month")
+  }
 
   return (
-    <div style={{backgroundColor: "#967bb6", height: "100vh"}} >
+    <div className="container" style={{backgroundColor: "#967bb6", height: "100vh"}} >
         <div
           className="calendar-header" 
-          style={{display: "flex", 
+          style={{
+              display: "flex",
               justifyContent: "center", 
               backgroundColor: "#d3d3d3",
+              width: "100vh",
               marginRight: "40px",
               marginLeft: "40px",
-              marginBottom: "5px"
+              marginBottom: "5px",
+              marginTop: "20px"
             }}
         >
-          <h1>Calendar</h1>
-          <div className="changeMonth" style={{}}></div>
+          <h1 className="Calendar"
+          >Calendar</h1>
+          <div className="changeMonth" 
+              style={{
+                display: "flex", 
+                flexDirection: "row", 
+                alignItems: "center",
+                marginLeft: "50px"
+            }}
+            >
+            <button className="left-click" onClick={ChangeToPastMonth}>←</button>
+            <p>Current Month: {dateMonthYear.month}</p>
+            <button className="right-click" onClick={ChangeToFutureMonth}>→</button>
+          </div>
         </div>
         <CalendarMonth month={dateMonthYear.month} year={dateMonthYear.year}></CalendarMonth>;
     </div>
