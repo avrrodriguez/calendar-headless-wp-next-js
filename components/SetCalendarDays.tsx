ded0981b2120fd83639b9f6ba539f9cceb18eeba
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-07-12 13:55:13 -0400

    makes seteventdays return divs based on how many events there are

--- components/SetCalendarDays.tsx
+++ components/SetCalendarDays.tsx
@@ -6,7 +6,7 @@ import SetEventDays from "./SetEventDays"
 export function SetCalendarDays({month, year, calendarDayElementId, calendarCellNumber, events}: dateDateIdAndEvents): JSX.Element {
   const date: Date = new Date(`${month} 1, ${year}`)
   const startMonthDay: number = date.getDay()
-  var calendarDayNumber = calendarCellNumber - startMonthDay
+  const calendarDayNumber = calendarCellNumber - startMonthDay
   var endMonthDay = GetCalendarMonthDays(`${month}`)
   
   const col: number = parseInt(calendarDayElementId.slice(4, 5))
@@ -14,25 +14,27 @@ export function SetCalendarDays({month, year, calendarDayElementId, calendarCell
   
   const eventsOfTheDay = events
   .filter((calendarEvent) => 
-  parseInt(calendarEvent.event.eventStartDate) >= calendarDayNumber || parseInt(calendarEvent.event.eventEndDate) <= calendarDayNumber)
+  parseInt(calendarEvent.event.eventStartDate) <= calendarDayNumber && parseInt(calendarEvent.event.eventEndDate) >= calendarDayNumber)
+
+  const isCalendarDay: boolean = calendarDayNumber > 0 && calendarDayNumber <= endMonthDay
+  const isDayOfEvent: boolean = eventsOfTheDay.length > 0
 
   function displayDay() {
     const calendarDay = document.getElementById(`col-${col}-row-${row}`)
     
     if (((row >= startMonthDay && col == 0) || col > 0) && (calendarDayNumber <= endMonthDay)) {
-      calendarDay.innerText = `${calendarDayNumber}`
       calendarDay.style.backgroundColor = "white"
     } else {
-      calendarDay.textContent = ""
       calendarDay.style.backgroundColor = "gray"
     }
-
-    if(eventsOfTheDay.length > 0) {SetEventDays(eventsOfTheDay)}
   }
+
   useEffect(() => {displayDay()}, [month])
 
   return (
-    <div>
+    <div style={{}}>
+      {isCalendarDay ? (calendarDayNumber) : ""}
+      {isDayOfEvent ? (<div><SetEventDays events={eventsOfTheDay}/></div>) : <></>}
     </div>
   )
 }
--- components/SetEventDays.tsx
+++ components/SetEventDays.tsx
@@ -1,32 +1,38 @@
 import { dateDateIdAndEvents, EventType } from "types/graphql/graphql"
 
-export default function SetEventDays(events: EventType[]): void {
+type typeofEvents = {
+  events: EventType[]
+}
 
-  const eventStartDay = parseInt(events[0].event.eventStartDate.slice(0, 2))
-  console.log(parseInt(events[0].event.eventStartDate.slice(0, 2)))
-  const eventEndDay = parseInt(events[0].event.eventEndDate.slice(0, 2))
-  const eventTitle = events[0].title
-
+export default function SetEventDays({ events }: typeofEvents): JSX.Element {
   function handleEventClick(){
     window.location.href = `/calendar/${events[0].uri}`
   }
 
-  const setEventDays = 
-  [...Array(6).keys()].map((col) => {
-    [...Array(7).keys()].map((row)=>{
-      const calendarDay = document.getElementById(`col-${col}-row-${row}`)
-      if (parseInt(calendarDay.innerText) >= eventStartDay && parseInt(calendarDay.innerText) <= eventEndDay) {
+  var keyCount: number = 0
 
-        calendarDay.style.backgroundColor = "rgba(255, 0, 0, 0.4)"
-        const newPElement = document.createElement("p");
-        const addTextToPElement = document.createTextNode(`Event Name: ${eventTitle}`);
-        newPElement.appendChild(addTextToPElement)
-        calendarDay.appendChild(newPElement)
-
-        calendarDay.style.cursor="pointer"
-
-        calendarDay.addEventListener("click", handleEventClick)
-      }
-    })
-  })
+  return (
+    <div>
+      {events.map((calendarDayEvent) => {
+        return (
+          <div 
+            key={`${calendarDayEvent.event.eventName}-${keyCount}`}
+            onClick={()=>{handleEventClick()}} 
+            style={{
+              cursor: "pointer", 
+              backgroundColor: "rgba(255, 0, 0, 0.4)",
+              height: "30px",
+              fontSize: "10px",
+              borderRadius: "10px",
+              overflow: "hidden",
+              textOverflow: "ellipsis"
+            }}
+            >
+            <p style={{margin: "0px"}}><b>{calendarDayEvent.event.eventName}:</b></p>
+            <p style={{margin: "0px"}}>{calendarDayEvent.event.eventDescription}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
